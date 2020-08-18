af2c3cb50ed89d33dc3c8dee5299492e48931864
Carl Michael Gaspar <carl.michael.gaspar@icloud.com>
2020-08-18 14:29:51 +0400

    new data set for lab 1 (fake employment data)

---
+++ data/make-employment-data.R
@@ -0,0 +1,23 @@
+status <- c("full-time", "part-time", "unemployed", "not looking")
+
+prob_male <- c(30, 55, 10, 5)
+n_male <- 40
+employment_male <- sample(status, n, replace=TRUE, prob=prob)
+
+prob_female <- c(20, 60, 5, 15)
+n_female <- 40
+employment_female <- sample(status, n, replace=TRUE, prob=prob)
+
+employment <- c(employment_male, employment_female)
+gender <- rep(c("male", "female"), each=n)
+
+data <- data.frame(ID=seq(n), gender=gender, employment=employment)
+write.csv(data, file = "employment.csv", row.names = FALSE)
+
+# remember to append many rows of ",," after making this file!
+# for lesson about closely examining your data files.
+# also specific info about filtering in jamovi.
+
+print("remember to append many rows of ",," after making this file!")
+print("for lesson about closely examining your data files.")
+print("# also specific info about filtering in jamovi.")
