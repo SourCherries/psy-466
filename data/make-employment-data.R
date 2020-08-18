5c67a4c69d564cbc2d697edae5965a731bedc44e
Carl Michael Gaspar <carl.michael.gaspar@icloud.com>
2020-08-18 15:28:00 +0400

    increased number of subjects in employment data, to make auto examination of levels etc more realistic

--- data/make-employment-data.R
+++ data/make-employment-data.R
@@ -1,17 +1,18 @@
 status <- c("full-time", "part-time", "unemployed", "not looking")
 
 prob_male <- c(30, 55, 10, 5)
-n_male <- 40
-employment_male <- sample(status, n, replace=TRUE, prob=prob)
+n_male <- 200
+employment_male <- sample(status, n_male, replace=TRUE, prob=prob)
 
 prob_female <- c(20, 60, 5, 15)
-n_female <- 40
-employment_female <- sample(status, n, replace=TRUE, prob=prob)
+n_female <- n_male
+employment_female <- sample(status, n_female, replace=TRUE, prob=prob)
+
 
 employment <- c(employment_male, employment_female)
-gender <- rep(c("male", "female"), each=n)
+gender <- rep(c("male", "female"), each= n_male)
 
-data <- data.frame(ID=seq(n*2), gender=gender, employment=employment)
+data <- data.frame(ID=seq(n_male*2), gender=gender, employment=employment)
 write.csv(data, file = "employment.csv", row.names = FALSE)
 
 # remember to append many rows of ",," after making this file!
