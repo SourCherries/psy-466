5ae9b67a83fae03c0e69d21f29ca61d985cac33c
Carl Michael Gaspar <carl.michael.gaspar@icloud.com>
2020-08-18 14:52:25 +0400

    fixed ID for employment data. previously ID repeated across gender. now each ID unique as it should be.

--- data/make-employment-data.R
+++ data/make-employment-data.R
@@ -11,13 +11,13 @@ employment_female <- sample(status, n, replace=TRUE, prob=prob)
 employment <- c(employment_male, employment_female)
 gender <- rep(c("male", "female"), each=n)
 
-data <- data.frame(ID=seq(n), gender=gender, employment=employment)
+data <- data.frame(ID=seq(n*2), gender=gender, employment=employment)
 write.csv(data, file = "employment.csv", row.names = FALSE)
 
 # remember to append many rows of ",," after making this file!
 # for lesson about closely examining your data files.
 # also specific info about filtering in jamovi.
 
-print("remember to append many rows of ",," after making this file!")
+print("remember to append many rows of ,, after making this file!")
 print("for lesson about closely examining your data files.")
 print("# also specific info about filtering in jamovi.")
